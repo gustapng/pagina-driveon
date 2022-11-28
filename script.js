ae0af0135ff467fcc61a19386ffa71f1073d919f
gustapng <gustavoferreira@gmail.com>
2022-11-28 20:54:41 -0300

    animations update

---
+++ script.js
@@ -0,0 +1,7 @@
+//efeito scroll reveal
+
+window.sr = ScrollReveal({ reset:true });
+
+sr.reveal('.section2', {duration: 1000});
+sr.reveal('.section5', {duration: 2000});
+sr.reveal('.section6', {duration: 2000});
